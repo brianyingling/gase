fd48e9207576cc92b3080dc74698beaa8eaffb31
Brian Yingling <brian_yingling@yahoo.com>
2013-02-02 19:25:52 -0500

    modified main.rb

--- main.rb
+++ main.rb
@@ -3,16 +3,17 @@ require 'pry-debugger'
 require 'rainbow'
 require 'yahoofinance'
 require 'functions'
+require_relative 'data'
 
 while response != 'q'.downcase
   menu
   case response
-  when 1 then ## call function
-  when 2 then ## call function
-  when 3 then ## call function
-  when 4 then ## call function
-  when 5 then ## call function
-  when 6 then ## call function
+  when '1' then ## call function
+  when '2' then ## call function
+  when '3' then ## call function
+  when '4' then ## call function
+  when '5' then ## call function
+  when '6' then ## call function
   else then menu_error
   end
 end
